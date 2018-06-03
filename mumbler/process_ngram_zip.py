b2d05a9b36603c140cb88a27d1425f1f0e9d0513
Walter Burge <walt.burge@berkeley.edu>
2018-06-02 21:19:48 -0700

    logic to zip and delete all JSON and CSV file and split letters_counts into letter_tree files, then zipped

--- mumbler/process_ngram_zip.py
+++ mumbler/process_ngram_zip.py
@@ -1,15 +1,18 @@
 import codecs
 from configparser import ConfigParser
+import glob
 import json
 import os
 import re
 import sys
+import time
 import zipfile
 
 import requests
 
 DATA_DIR = "./data"
-PARSE_FILE_NAME_PATTERN = DATA_DIR+"/{}.{}.parse.csv"
+JSON_DIR = DATA_DIR+"/json"
+PARSE_CSV_FILE_NAME_PATTERN = DATA_DIR+"/{}.{}.parse.csv"
 IGNORED_LINES_PATTERN = DATA_DIR+"/ignored.lines.{}.csv"
 
 # The following regex template matches a single starting character, specified per node, followed by any characters that would match for any node,
@@ -25,7 +28,7 @@ def process_zip_file(zip_file_path, letters_words_counts):
     global node_regex
     global ignored_lines_file
 
-    with zipfile.ZipFile(zip_file_path) as zip_file:
+    with zipfile.ZipFile(zip_file_path, "r") as zip_file:
         for csv_filename in zip_file.namelist():
             with zip_file.open(csv_filename) as csv_file:
                 for line in csv_file:
@@ -45,25 +48,80 @@ def process_zip_file(zip_file_path, letters_words_counts):
                     else:
                         write_other_node_file(line)
 
-    ignored_lines_file.flush()
-    ignored_lines_file.close()
+def close_files():
 
+    PARSE_ZIP_FILE_NAME_PATTERN = DATA_DIR+"/{}.{}.parse.csv.zip"
+
+    global ignored_lines_file
+    global other_nodes_csv_files
+
+    if ignored_lines_file:
+        ignored_lines_file.flush()
+        ignored_lines_file.close()
+
+    if other_nodes_csv_files:
+        for csv_file in other_nodes_csv_files.itervalues():
+            csv_file.flush()
+            csv_file.close()
+
+    # iterate over all the parse files for other nodes, put them into zips and delete the original files
+    for path in glob.glob(DATA_DIR+"/gpfs?."+node_id+".parse.csv"):
+        zip_file_path = path + ".zip"
+
+        specific_path = path + "." + str(time.time())
+        os.rename(path, specific_path)
+
+        with zipfile.ZipFile(zip_file_path, "a") as zip_file:
+            zip_file.write(specific_path)
+
+        os.remove(specific_path)
+
+    # iterate over all the JSON letter files, put them into zips and delete the original files
+    zip_file_path = str(JSON_DIR+"/letter_trees." + node_id + "." + str(time.time()) + ".zip")
+    for path in glob.glob(JSON_DIR+"/*_tree."+node_id+".*.json"):
+
+        with zipfile.ZipFile(zip_file_path, "a") as zip_file:
+            zip_file.write(path)
+
+        os.remove(path)
+
+    # zip the ignored lines file and delete the original
+    ignored_lines_filename = IGNORED_LINES_PATTERN.format(node_id)
+
+    if os.path.exists(ignored_lines_filename):
+        ignored_zip = ignored_lines_filename + ".zip"
+
+        # rename the ignored lines file, so that it can be collected with others for this node in a zip
+        ignored_specific_filename = ignored_lines_filename + "." + str(time.time())
+        os.rename(ignored_lines_filename, ignored_specific_filename)
+
+        with zipfile.ZipFile(ignored_zip, "a") as zip_file:
+            zip_file.write(ignored_specific_filename)
+
+        os.remove(ignored_specific_filename)
 
 def write_other_node_file(line):
 
     global ignored_lines_file
+    global other_nodes_csv_files
+    global other_nodes_zip_files
 
     matched = False
-    PARSE_FILE_NAME_PATTERN = DATA_DIR+"/{}.{}.parse.csv"
-    IGNORED_LINES_PATTERN = DATA_DIR+"/ignored.lines.{}.csv"
     ignored_lines_filename = IGNORED_LINES_PATTERN.format(node_id)
 
     for some_node_id, some_regex in other_nodes_regex.iteritems():
         other_regex = re.compile(NGRAM_REGEX.format(some_regex, some_regex))
         if other_regex.match(line):
             matched = True
-            with codecs.open(PARSE_FILE_NAME_PATTERN.format(some_node_id, node_id), "a+",encoding="utf8") as other_file:
-                other_file.write(line)
+
+            other_node_csv_file_path = PARSE_CSV_FILE_NAME_PATTERN.format(some_node_id, node_id)
+            other_node_csv_file = other_nodes_csv_files.get(some_node_id)
+
+            if not other_node_csv_file:
+                other_node_csv_file = open(other_node_csv_file_path, "a+")
+                other_nodes_csv_files[some_node_id] = other_node_csv_file
+
+            other_node_csv_file.write(line)
 
     if not matched:
         if not ignored_lines_file:
@@ -116,17 +174,38 @@ def process_ngram(first_word, second_word, match_count, letters_words_counts):
             second_word_counts["count"] = second_word_counts["count"] + match_count
 
 
+def split_by_letter(letter_words_counts):
+
+    letter_trees = {}
+
+    for word, counts in letter_words_counts.iteritems():
+
+        letter_key = word[0]
+
+        if not re.match("[a-zA-Z]", letter_key):
+            letter_key = "punct"
+
+        letter_dict = letter_trees.get(letter_key)
+
+        if not letter_dict:
+            letter_dict = {}
+            letter_trees[letter_key] = letter_dict
+
+        letter_dict[word] = counts
+
+    for letter, tree in letter_trees.iteritems():
+        filepath = JSON_DIR+"/"+letter+"_tree."+node_id+"."+str(time.time())+".json"
+        with codecs.open(filepath, "w", "utf8") as dict_file:
+            dict_file.write(json.dumps(tree))
+
+
 def read_config():
 
     global config
     global letters_words_counts
     global node_regex
     global other_nodes_regex
-    global ignored_lines_filename
-    global ignored_lines_file
-
-    global ignored_lines_file
-
+    global ignored_lines_filenamea
 
     config = ConfigParser()
     letters_words_counts = {}
@@ -160,15 +239,19 @@ if __name__ == "__main__":
 
     global config
     global letters_words_counts
+    global ignored_lines_zip_files
+    global other_nodes_zip_files
+    global other_nodes_csv_files
 
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
+    ignored_lines_zip_files = {}
+    other_nodes_zip_files = {}
+    other_nodes_csv_files = {}
 
     read_config()
 
     node_id = config.get("node", "node_id")
 
-    json_file_name = DATA_DIR+"/letters_words_counts."+node_id+".json"
+    json_file_name = JSON_DIR+"/letters_words_counts."+node_id+".json"
 
     # Check if the node-specific dictionary JSON file exists and open for append if so
     if os.path.exists(json_file_name):
@@ -193,5 +276,9 @@ if __name__ == "__main__":
         print("\nUsage: python process_ngram_zip <zip file path>")
     else:
         process_zip_file(sys.argv[1], letters_words_counts)
-        with codecs.open(DATA_DIR + "/letters_words_counts."+node_id+".json", "w", "utf8") as dict_file:
-            dict_file.write(json.dumps(letters_words_counts))
+
+        split_by_letter(letters_words_counts)
+
+        close_files()
+
+
