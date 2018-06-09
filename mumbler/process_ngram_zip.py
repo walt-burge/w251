cace30ef1b30a5010fafb23af24049cb3d29353c
Walter Burge <walt.burge@berkeley.edu>
2018-06-08 19:17:45 -0700

    replaced a get of node_regex during main

--- mumbler/process_ngram_zip.py
+++ mumbler/process_ngram_zip.py
@@ -288,7 +288,7 @@ if __name__ == "__main__":
     read_config()
 
     node_id = config.get("node", "node_id")
-    node_regex = config.get("")
+    node_regex = config.get("patterns", node_id+".regex")
 
     json_file_name = JSON_DIR+"/letters_words_counts."+node_id+".json"
 
