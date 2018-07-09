158f412c3fadcd4d694116e70463ffb507f6d7a3
root <root@spark1.mids.com>
2018-07-09 12:34:09 -0500

    Many changes, got Twitter Stream with Tweet DataFrames roughly functional

--- twitter_popularity/TwitterPopularityMonitor/twitter_popularity.scala
+++ twitter_popularity/TwitterPopularityMonitor/twitter_popularity.scala
@@ -7,10 +7,10 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 
 import org.apache.spark.sql._
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.Dataset
 
 import twitter4j.Status
 import twitter4j.User
@@ -26,38 +26,19 @@ object TwitterPopularityMonitor extends App {
         return hashtags.map(hashtag => (hashtag, tweetUser)).toSeq
     }
 
-    case class Tweet(hashTweet: Array[String], user: twitter4j.User, refs: Array[String])
+    case class Tweet(hashTweet: Array[String], user:String, refs: Array[String])
         
-    def remapHashtagsTweetRefs(rdd:RDD[twitter4j.Status]) : Dataset[Tweet] = {
-	// Get the singleton instance of SparkSession
-	val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate() 
-	import spark.implicits._
+    def Record(status:Status) : Tweet = {
+	val userObj = status.getUser()
+	val userName = if ( userObj != null ) { userObj.getScreenName() } else { null }
 
-	// Convert RDDs of the words DStream to DataFrame and run SQL query
-    	rdd.foreach{case (status:Status) =>
-     
-      		// Convert RDD[twitter4j.Status] to RDD[case class]
-      		val tweets = rdd.map(status => Record(status))
+	val tmpHashtags = status.getText.split(" ").filter(_.startsWith("#"))
+	val hashtags = if (tmpHashtags.length > 0) { tmpHashtags } else { null }
 
-      		//val tweetsDataFrame = rdd.map(status => Tweet(status.getText.split(" ").filter(_.startsWith("#")),
-		//					status.getUser(),
-		//				status.getText.split(" ").filter(_.startsWith("@"))).toDF())
+	val tmpTweetRefs = status.getText.split(" ").filter(_.startsWith("@"))
+	val tweetRefs = if (tmpTweetRefs.length > 0) { tmpTweetRefs } else { null }
 
-      		// Creates a temporary view using the DataFrame
-		val tweetsDataFrame = tweets.toDF
-      		tweetsDataFrame.createOrReplaceTempView("tweets")
-
-	}
-
-	// Aggregate tweet data in ttable using SQL and return it
-      	return spark.sql("select hashtag, user, refs from tweets group by hashtag")
-    }
-
-    def Record(status:Status) : RDD[Tweet] = {
-	val user = status.getUser()
-	val hashtags = status.getText.split(" ").filter(_.startsWith("#"))
-	val tweetRefs = status.getText.split(" ").filter(_.startsWith("@"))
-	hashtags.map{hashtag => (hashtag, user, tweetRefs)};
+	return Tweet(hashtags, userName, tweetRefs)
     }
         
     if (args.length < 4) {
@@ -92,30 +73,48 @@ object TwitterPopularityMonitor extends App {
     val stream = TwitterUtils.createStream(ssc, None, filters)
 
     val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
-    val hashtagsTweetRefs = stream.transform(status => remapHashtagsTweetRefs(status))
+    //val hashtagsTweetRefs = stream.transform(status => Record(status))
+
+    // Get the sqlContext from the stream?
+    // Get the singleton instance of SparkSession
+    //val spark = SparkSession.builder.config(sparkConf).getOrCreate() 
+    //import spark.implicits._
+    val spark= SparkSession.builder().getOrCreate()
+    import spark.implicits._
+
+    val hashtagsTweetRefs = stream.map(status => Record(status))
+
+    hashtagsTweetRefs.foreachRDD{ tweet =>
+	// Get the singleton instance of SparkSession
+	val spark = SparkSession.builder.config(tweet.sparkContext.getConf).getOrCreate()
+	import spark.implicits._
+
+	val tweetDF = tweet.toDF("hashtags", "user", "refs")
+
+	// Create a temporary view
+	tweetDF.createOrReplaceTempView("tweets")
+
+	val summaryDF = spark.sql("SELECT * from tweets WHERE LENGTH(hashtags)>0")
+	print("SummaryDF: ")
+	summaryDF.show()
+    }
 
     val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
                      .map{case (topic, count) => (count, topic)}
                      .transform(_.sortByKey(false))
-		     .map{case (count, topic) => (topic, count)}
-		     .join(hashtagsTweetRefs)
-		     .map{case (topic, (count, tweetRefs)) => (count, (topic, tweetRefs))}
 
 
     val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
                      .map{case (topic, count) => (count, topic)}
                      .transform(_.sortByKey(false))
-		     .map{case (count, topic) => (topic, count)}
-		     .join(hashtagsTweetRefs)
-		     .map{case (topic, (count, tweetRefs)) => (count, (topic, tweetRefs))}
 
 
     // Print popular hashtags
     topCounts60.foreachRDD(rdd => {
       val topList = rdd.take(10)
-
       println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, hashtag, tweeters, tweetrefs) => { println("%s (%s tweets), tweeters: %s, tweetrefs: %s".format(hashtag, count, tweeters, tweetrefs))}}
+
+      topList.foreach{case (count, hashtag) => { val tweeters=null; val tweetrefs=null; println("%s (%s tweets), tweeters: %s, tweetrefs: %s".format(hashtag, count, tweeters, tweetrefs))}}
     })
 
     topCounts10.foreachRDD(rdd => {
